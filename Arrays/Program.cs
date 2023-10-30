faef5c31e5489f8fa39db2ef0f1aa7c9c2599a88
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-10-30 09:27:12 -0300

    update

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,6 +8,10 @@ namespace Arrays
 {
     internal class Program
     {
+        static int[] ammo;
+
+        static int currentWeapon;
+         
         static void Main(string[] args)
         {
             Console.WriteLine("Arrays");
@@ -15,9 +19,59 @@ namespace Arrays
             Console.WriteLine("---------------");
             Console.WriteLine();
 
+            // single weapon
+            // weapon ammo
+            // int ammo;
+            // ammo = 0;
+
+
+            // multiple weapons 
+            // weapon ammo (for all weapons)
+            // - pistol
+            // - shotgun
+            // - spreader
+            // - rocket launcher
+            //int pistolAmmo;
+            //int shotgunAmmo;
+            //int spreaderAmmo;
+            //int rocketLauncherAmmo;
+            //string currentWeapon;
+            // Fire()
+
+
+            // Multiple weapons with arrays
+
+            // 0 - pistol
+            // 1 - shotgun
+            // 2 - spreader
+            // 3 - rocket launcher
+
+            int[] ammo; // declaration
+                        // "int[]" = array
+            ammo = new int[4]; // instantiation (NEW) make sure there's memory, could fail!
+            
+            // initialization (MAX AMMO CHEAT)
+            ammo[0] = 50; // pistol
+            ammo[1] = 20; // shotgun
+            ammo[2] = 100; // spreader
+            ammo[3] = 6; // rocket launcher
+            int currentWeapon;
+            currentWeapon = 0; // pistol
+            // Fire(int currentWeapon)
+            // PickUp(int weaponType)
+
+
             Console.WriteLine();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey(true);
         }
+
+        static void Fire(int currentWeapon)
+        {
+            if (ammo[currentWeapon] <= 0) return;
+            ammo[currentWeapon] = ammo[currentWeapon] - 1;
+        }
+
     }
+
 }
